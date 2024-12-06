494605bf8e7d736544d3697c1ac0608b7b72edeb
Gudhe Jagadeesh <jagadeesh8808@gmail.com>
2024-12-06 23:47:14 +0530

    react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Main from './main';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default App;
